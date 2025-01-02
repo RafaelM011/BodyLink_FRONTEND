892bcd0a5fe0a4bdd9065efdd0cf13faf105ab0b
AdrifeCova <adrife2000@hotmail.com>
2025-01-02 17:42:02 -0600

    feat: removed test route

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ function App() {
     <Routes>
       <Route path='/' element={<DashBoardView />}>
         <Route path='/' element={<HomePage />} />
-        <Route path='/pacientes' element={<HomePage />} />
       </Route>
     </Routes>
   );
